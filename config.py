4bfc7375313a98ff24f8cecf5d8221dbfef14528
♥⁠╣Sivam_uv╠⁠♥ <123348827+Botprim@users.noreply.github.com>
2025-03-27 14:37:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
 import os
+from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
+
+# Get environment variables
 BOT_TOKEN = os.getenv("7701780212:AAE0mWHCbSkeRlOVsv_iMBZXBQ4WjtDMzps")
 CHANNEL_USERNAME = os.getenv("tomjerbackup")
+
+if not BOT_TOKEN:
+    raise ValueError("❌ Error: BOT_TOKEN is missing! Set it in environment variables.")
+
+if not CHANNEL_USERNAME:
+    raise ValueError("❌ Error: CHANNEL_USERNAME is missing! Set it in environment variables.")
