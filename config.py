ba282965a8362a94ce79da3997f36cc12494c5c8
♥⁠╣Sivam_uv╠⁠♥ <123348827+Botprim@users.noreply.github.com>
2025-03-27 15:49:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
+# Load environment variables from .env file
 load_dotenv()
 
-# Get environment variables
-BOT_TOKEN = os.getenv("7701780212:AAE0mWHCbSkeRlOVsv_iMBZXBQ4WjtDMzps")
-CHANNEL_USERNAME = os.getenv("tomjerbackup")
+# Fetch environment variables
+BOT_TOKEN = os.getenv("BOT_TOKEN" "7701780212:AAE0mWHCbSkeRlOVsv_iMBZXBQ4WjtDMzps")
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME" "tomjerbackup")
 
+# Error handling if environment variables are missing
 if not BOT_TOKEN:
     raise ValueError("❌ Error: BOT_TOKEN is missing! Set it in environment variables.")
 
 if not CHANNEL_USERNAME:
     raise ValueError("❌ Error: CHANNEL_USERNAME is missing! Set it in environment variables.")
+
